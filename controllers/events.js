90f4eed1efe0a1cfbdbbca28bf2faa83b13932aa
glenndenning061 <kelly.glenn.g@gmail.com>
2015-12-02 18:17:00 +0000

    This has the code required for google analytics

--- controllers/events.js
+++ controllers/events.js
@@ -102,7 +102,7 @@ function saveEvent(request, response){
       title: request.body.title,
       location: request.body.location,
       image: request.body.image,
-      date: new Date(),
+      date: Date(),
       attending: []
     };
     events.all.push(newEvent);
